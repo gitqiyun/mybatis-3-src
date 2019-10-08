ed05d7996ccfdac379be1d7ef51ef3c8513161f3
gitqiyun <1114260199@qq.com>
2019-10-08 09:04:29 +0800

    hh

--- src/main/java/domain/MybatisTest.java
+++ src/main/java/domain/MybatisTest.java
@@ -17,10 +17,13 @@ public class MybatisTest {
         //sqlSession用于使用MyBatis的主要Java接口
         SqlSession sqlSession=sqlSessionFactory.openSession();
         try {
+            //生成动态代理mapper，实际执行的是sqlsession的方法
             BlogMapper blogMapper = sqlSession.getMapper(BlogMapper.class);
             Integer id=101;
             String name="tom";
+            //具体实现由sqlsession实现，从configuration获取sql，再获取数据库连接connection执行sql
             Blog blog = blogMapper.selectBlogById(id,name);
+            Blog blog2 = blogMapper.selectBlogById(id,name);
             System.out.println(blog.getId());
         } finally {
             sqlSession.close();
